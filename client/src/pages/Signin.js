f48d2d297acde1ac33a1e2703dc0ce0d756a29f6
Abega <ebenezermelkamu@gmail.com>
2022-03-17 15:56:27 +0300

    Add signin component

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,7 +1,94 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  FormControlLabel,
+  Checkbox,
+  Grid,
+  Link,
+} from "@mui/material";
 
 export default function Signin() {
   return (
-    <div>Signin</div>
-  )
+    <div>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Signin
+        </Typography>
+        <Box component="form" sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Enter your email address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Enter your password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Signin
+          </Button>
+
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forget password
+              </Link>
+            </Grid>
+
+            <Grid item>
+              <Link href="#" variant="body2">
+                If you don't have an account signup here
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Button type="button" fullWidth variant="outlined" sx={{ mt: 3, mb: 2 }}>
+            Signin With Github
+          </Button>
+
+          <Button type="button" variant="outlined" sx={{ mt: 3, mb: 2 }}>
+            Signin With Google
+          </Button>
+        </Box>
+      </Box>
+    </div>
+  );
 }
